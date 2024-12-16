50c2d52c087aac881c37b7299b084b371406b58a
Agenor Junior <66803750+Agenor-Junior@users.noreply.github.com>
2024-12-16 13:43:29 -0500

    Update w8p2.c

--- w08/w8p2.c
+++ w08/w8p2.c
@@ -1,4 +1,4 @@
-/*/////////////////////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////
 						  Workshop - #8 (P2)
 Full Name  : Agenor Dionizio da SIlva Junior	
 Student ID#: 138121223
@@ -9,7 +9,7 @@ Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
 shared with any other student or 3rd party content provider. This submitted
 piece of work is entirely of my own creation.
-/////////////////////////////////////////////////////////////////////////*/
+////////////////////////////////////////////////////////////////////////*/
 // System Libraries
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
